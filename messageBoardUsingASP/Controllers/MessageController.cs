55de90718dda462b168fa159186ae9c4ef80abba
dannypc1628 <h152074716@gmail.com>
2018-03-12 23:06:52 +0800

    設定startup並加入Controller與View

---
+++ messageBoardUsingASP/Controllers/MessageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace messageBoardUsingASP.Controllers
+{
+    public class MessageController:Controller
+    {
+        public IActionResult Board()
+        {
+            var response = View();
+            return response;
+        }
+    }
+}
